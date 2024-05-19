4333f3bc00b418768eaf2728b6ad6ea8912d786d
0xAckerMan <joelkores1@gmail.com>
2024-05-19 04:54:49 +0300

    The rating sevice controller func

---
+++ rating/internal/controller/rating/controller.go
@@ -0,0 +1,46 @@
+package rating
+
+import (
+	"context"
+	"errors"
+
+	"github.com/0xAckerMan/movieapp-ms/rating/internal/repository"
+	model "github.com/0xAckerMan/movieapp-ms/rating/pkg"
+)
+
+// Defines the error for a not found record
+var ErrNotFound = errors.New("Rating not found for a record")
+
+type ratingRepository interface{
+    Get(ctx context.Context, recordId model.RecordID, recordType model.RecordType) ([]model.Rating, error)
+    Put(ctx context.Context, recordID model.RecordID, recordType model.RecordType, rating *model.Rating) error
+}
+
+// Controller defines a rating service controller
+type Controller struct{
+    repo ratingRepository
+}
+
+func New(repo ratingRepository) *Controller{
+    return &Controller{repo}
+}
+
+func (c *Controller) GetAggregatedRating (ctx context.Context, recordID model.RecordID, recordType model.RecordType) (float64, error){
+    ratings, err := c.repo.Get(ctx,recordID,recordType)
+    if err != nil && err == repository.ErrNotFound{
+        return 0, ErrNotFound
+    } else if err != nil{
+        return 0, err
+    }
+
+    sum  := float64(0)
+
+    for _, r := range ratings{
+        sum += float64(r.Value)
+    }
+    return sum / float64(len(ratings)), nil
+}
+
+func (c *Controller) PutRating(ctx context.Context, recordID model.RecordID, recordType model.RecordType, rating *model.Rating) error {
+    return c.repo.Put(ctx,recordID,recordType,rating)
+}
